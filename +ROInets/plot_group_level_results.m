fa180b8fdd80612288f165fcb088359e04fe01e7
Giles Colclough <giles.colclough@gmail.com>
2016-03-02 12:44:01 +0000

    Allowed plot group results to run on rest
    Signed-off-by: Giles Colclough <giles.colclough@gmail.com>

--- +ROInets/plot_group_level_results.m
+++ +ROInets/plot_group_level_results.m
@@ -38,28 +38,28 @@ function plot_group_level_results(correlationMats, Settings, netType, correction
 if ~iscell(correlationMats),
 	error([mfilename ':NonCellInput'], ...
 		  'Expecting a cell array of structures as first input. \n');
-end%if
+end%if	
+
+
 isTask = isfield(correlationMats{1}, 'firstLevel') ...
 	             && strcmpi(Settings.paradigm, 'task');
-if ~isTask,
-	error([mfilename ':NotTaskInput'], ...
-		  'Expecting correlationMats to be output of task analysis. \n');
-end%if	
 
 nFreqs                    = Settings.nFreqBands;
 nFreqsCheck               = length(correlationMats);
-nFirstLevelContrasts      = length(Settings.SubjectLevel.contrasts);
-nFirstLevelContrastsCheck = length(correlationMats{1}.firstLevel);
 nGroupLevelContrasts      = ROInets.rows(Settings.GroupLevel.contrasts);
 nGroupLevelContrastsCheck = size(correlationMats{1}.groupLevel(1).correlation.T,3);
 
 assert(nFreqsCheck               == nFreqs               && ...
-	   nFirstLevelContrastsCheck == nFirstLevelContrasts && ...
 	   nGroupLevelContrastsCheck == nGroupLevelContrasts,   ...
 	   [mfilename ':MatSettingsMismatch'],                  ...
 	   'The Settings file and CorrelationMats file do not seem to match up. \n');
 
-nROIs  = ROInets.rows(correlationMats{1}.firstLevel(1).cope.correlation);
+if isTask,
+   nROIs  = ROInets.rows(correlationMats{1}.firstLevel(1).cope.correlation);
+else
+   nROIs = ROInets.rows(correlationMats{1}.correlation);
+end%if
+
 validatestring(netType, {'correlation', 'partialCorrelation', ...
 					     'partialCorrelationRegularized'},    ...
 	           mfilename, 'netType', 3);
@@ -75,31 +75,93 @@ if nargin >= 6 && ~isempty(ROIorder),
 else
 	ROIorder = 1:nROIs;
 end%if
-if nargin >= 7 && ~isempty(firstLevelConsToPlot),
-	assert(all(ismember(firstLevelConsToPlot, 1:nFirstLevelContrasts)));
-else
-	firstLevelConsToPlot = 1:nFirstLevelContrasts;
-end%if
 if nargin >= 8 && ~isempty(groupLevelConsToPlot),
 	assert(all(ismember(groupLevelConsToPlot, 1:nGroupLevelContrasts)));
 else
 	groupLevelConsToPlot = 1:nGroupLevelContrasts;
 end%if
-%% Let's plot away
-FONTSIZE = 15;
-for iFreq = 1:nFreqs,
-	for iConFirst = firstLevelConsToPlot,
-			matBase = correlationMats{iFreq}.groupLevel(iConFirst).(netType);
+
+
+	FONTSIZE = 15;
+	
+
+if isTask,
+	nFirstLevelContrasts      = length(Settings.SubjectLevel.contrasts);
+	nFirstLevelContrastsCheck = length(correlationMats{1}.firstLevel);
+	assert(nFirstLevelContrastsCheck == nFirstLevelContrasts, ...
+	       [mfilename ':FirstLevelMismatch'],                 ...
+	       'The Settings file and CorrelationMats file do not seem to match up. \n');
+	if nargin >= 7 && ~isempty(firstLevelConsToPlot),
+		assert(all(ismember(firstLevelConsToPlot, 1:nFirstLevelContrasts)));
+	else
+		firstLevelConsToPlot = 1:nFirstLevelContrasts;
+	end%if
+
+	%% Let's plot away
+	for iFreq = 1:nFreqs,
+		for iConFirst = firstLevelConsToPlot,
+				matBase = correlationMats{iFreq}.groupLevel(iConFirst).(netType);
+			for iConGroup = groupLevelConsToPlot,
+				CAXIS = [0 3];
+				switch correctionType
+					case 'T'
+						plotMat = matBase.(correctionType)(ROIorder,ROIorder,iConGroup);
+						CAXIS = [0 15];
+
+					case {'p', 'FWEp'}
+						plotMat = -log10(matBase.(correctionType)(ROIorder,ROIorder,iConGroup));
+
+					case 'FDRh'
+						% threshold original p-values by FDR-corrected bound
+						h = matBase.(correctionType)(ROIorder,ROIorder,iConGroup);
+						p = matBase.p(ROIorder,ROIorder,iConGroup);
+						threshP       = p;
+						threshP(p~=h) = 1;
+						plotMat       = -log10(threshP);
+					otherwise
+						error([mfilename ':BadCorrectionType'], ...
+							  'Unrecognised correction type. \n');
+				end
+				plotTitle = sprintf('Freq band %d, First level contrast %d, Group level contrast %d, %s %s results', ...
+									iFreq, iConFirst, iConGroup, netType, correctionType);
+				figure('Name', plotTitle, 'Color', 'w');
+				imagesc(plotMat(ROIorder,ROIorder));
+				set(gca, 'YTick', 1:nROIs, 'YTickLabel', ROInames);
+				set(gca,...
+					'FontName', 'Helvetica', ...
+					'FontSize', FONTSIZE, ...
+					'Box', 'on', ...
+					'YGrid', 'off', ...
+					'XGrid', 'off', ...
+					'TickDir', 'in', ...
+					'TickLength', [0.005 0.005], ...
+					'XMinorTick', 'off', ...
+					'YMinorTick', 'off', ...
+					'XColor', [0.3 0.3 0.3], ...
+					'YColor', [0.3 0.3 0.3], ...
+					'LineWidth', 2);	
+				caxis(CAXIS);
+				colormap(bluewhitered);
+				axis square
+				colorbar;
+			end%for
+		end%for
+	end%for
+
+else % not task
+	%% Let's plot away
+	for iFreq = 1:nFreqs,
+		matBase = correlationMats{iFreq}.groupLevel.(netType);
 		for iConGroup = groupLevelConsToPlot,
 			CAXIS = [0 3];
 			switch correctionType
 				case 'T'
-			        plotMat = matBase.(correctionType)(ROIorder,ROIorder,iConGroup);
+					plotMat = matBase.(correctionType)(ROIorder,ROIorder,iConGroup);
 					CAXIS = [0 15];
-					
+
 				case {'p', 'FWEp'}
 					plotMat = -log10(matBase.(correctionType)(ROIorder,ROIorder,iConGroup));
-					
+
 				case 'FDRh'
 					% threshold original p-values by FDR-corrected bound
 					h = matBase.(correctionType)(ROIorder,ROIorder,iConGroup);
@@ -111,9 +173,9 @@ for iFreq = 1:nFreqs,
 					error([mfilename ':BadCorrectionType'], ...
 						  'Unrecognised correction type. \n');
 			end
-			plotTitle = sprintf('Freq band %d, First level contrast %d, Group level contrast %d, %s %s results', ...
-				                iFreq, iConFirst, iConGroup, netType, correctionType);
-		    figure('Name', plotTitle, 'Color', 'w');
+			plotTitle = sprintf('Freq band %d, Group level contrast %d, %s %s results', ...
+								iFreq, iConGroup, netType, correctionType);
+			figure('Name', plotTitle, 'Color', 'w');
 			imagesc(plotMat(ROIorder,ROIorder));
 			set(gca, 'YTick', 1:nROIs, 'YTickLabel', ROInames);
 			set(gca,...
@@ -135,8 +197,7 @@ for iFreq = 1:nFreqs,
 			colorbar;
 		end%for
 	end%for
-end%for
-
+end%if istask
 
 end%plot_froup_level_results
 % [EOF]
